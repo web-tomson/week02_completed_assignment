02c750d1e292811dff7970d6d5f5b73a582304fa
tommyansu002@gmail.com <tommyansu002@gmail.com>
2026-06-11 17:05:12 +0000

    week6

--- cse212-ww-student-template-main/week06/code/Node.cs
+++ cse212-ww-student-template-main/week06/code/Node.cs
@@ -11,7 +11,8 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
@@ -33,13 +34,19 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+
+        return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
